e60e4ee51043651bdbbaa27f438517711f4e8d40
Sunday Toluwani Dere <toluwanisundaydere@gmail.com>
2020-08-10 05:41:44 +0100

    todo api

---
+++ apis/views.py
@@ -0,0 +1,25 @@
+from rest_framework import viewsets
+
+from todos import models
+from .serializers import TodoSerializer
+
+
+class TodoViewSet(viewsets.ModelViewSet):
+    queryset = models.Todo.objects.all()
+    serializer_class = TodoSerializer
+
+"""
+from rest_framework import generics
+
+from todos import models
+from .serializers import TodoSerializer
+
+class ListTodo(generics.ListCreateAPIView):
+    queryset = models.Todo.objects.all()
+    serializer_class = TodoSerializer
+
+
+class DetailTodo(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Todo.objects.all()
+    serializer_class = TodoSerializer
+"""
